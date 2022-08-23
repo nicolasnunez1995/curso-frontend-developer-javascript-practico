354b642c40f4eb83d164a039264449c3ab558355
nicolasnunez <nicolaasnunez@gmail.com>
2022-08-23 16:49:17 -0300

    INDEX CON CARRITO Y PRODUCTDETAIL

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -15,9 +15,9 @@ function toggleDesktopMenu(){
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
-    if (!isAsideClosed) aside.classList.add('inactive');
+    if (!isAsideClosed) shoppingCartContainer.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
 }
 
@@ -26,7 +26,7 @@ function toggleCarritoAside() {
     
     if(!isMobileMenuClosed) mobileMenu.classList.add('inactive');
 
-    aside.classList.toggle('inactive');   
+    shoppingCartContainer.classList.toggle('inactive');   
 }
 
 const producList = [];
